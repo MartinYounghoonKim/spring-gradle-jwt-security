f90a28bc86ef70ed7cecc90ef9bd63f5a01f0615
martinyounghoonkim <hoons0131@gmail.com>
2020-03-02 22:58:54 +0900

    JWT 토큰 Provider 추가

---
+++ src/main/java/com/springgradlejwtsecurity/auth/configuration/JwtTokenProvider.java
@@ -0,0 +1,58 @@
+package com.springgradlejwtsecurity.auth.configuration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.springgradlejwtsecurity.auth.entity.Account;
+import io.jsonwebtoken.*;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.Optional;
+
+@Component
+@RequiredArgsConstructor
+public class JwtTokenProvider {
+    private final String ENCRYPT_STRING =  "humansApiToekn!@#$%^&*()";
+
+    private long A_HOUR = 1000L * 60 * 60;
+    private static final String DATA_KEY = "user";
+    private final UserDetailsService userDetailsService;
+
+    public String createToken (Account account) {
+        return Jwts.builder()
+                .setHeaderParam("typ", "JWT")
+                .setExpiration(new Date(System.currentTimeMillis() + A_HOUR))
+                .claim(DATA_KEY, account)
+                .signWith(SignatureAlgorithm.HS256, generateKey())
+                .compact();
+    }
+    private byte[] generateKey(){
+        byte[] key = null;
+        try {
+            key = ENCRYPT_STRING.getBytes("UTF-8");
+        } catch (Exception e) {
+        }
+
+        return key;
+    }
+    public boolean verifyToken(String token) {
+        Optional.ofNullable(token)
+                .orElseThrow(() -> new SignatureException("토큰 값은 필수입니다."));
+        try {
+            Jwts.parser().setSigningKey(generateKey()).parseClaimsJws(token);
+
+            return true;
+        } catch (Exception e) {
+            throw new RuntimeException("유효하지 않은 토큰입니다.");
+        }
+    }
+    public Jws<Claims> parseToken (String token) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        Jws<Claims> test = Jwts.parser().setSigningKey(generateKey()).parseClaimsJws(token);
+        Account account = objectMapper.convertValue(test.getBody().get("user"), Account.class);
+        System.out.println(account);
+
+        return test;
+    }
+}
