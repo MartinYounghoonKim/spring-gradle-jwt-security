0f0103055b74bae366a50b57706b1f7583c6c3fa
martinyounghoonkim <hoons0131@gmail.com>
2020-03-02 22:58:17 +0900

    초기 data 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
 INSERT INTO account (user_id, password, permission)
-VALUES ('Admin', 'admin', '1234', 100);
+VALUES ('admin', '1234', 100);
 
 INSERT INTO account (user_id, password, permission)
-VALUES ('Martin', 'martin', '1234', 50);
+VALUES ('martin', '1234', 50);
