2bbd2a1fd324729beed80ba0979f4ee718041869
martinyounghoonkim <hoons0131@gmail.com>
2020-03-03 23:19:24 +0900

    ControllerAdvice Class 리펙토링

--- src/main/java/com/springgradlejwtsecurity/auth/exception/ControllerAdvice/ControllerAdvice.java
+++ src/main/java/com/springgradlejwtsecurity/auth/exception/ControllerAdvice/ControllerAdvice.java
@@ -2,13 +2,18 @@ package com.springgradlejwtsecurity.auth.exception.ControllerAdvice;
 
 import com.springgradlejwtsecurity.auth.dto.WebResponseDto;
 import com.springgradlejwtsecurity.auth.exception.CustomException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class ControllerAdvice {
     @ExceptionHandler(CustomException.class)
-    public WebResponseDto accountIdSigninFailedException (CustomException e) {
+    public WebResponseDto customExceptionHandler (CustomException e) {
         return WebResponseDto.failure(e.getMessage());
+    }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public WebResponseDto methodArgumentNotValidExceptionHandler (MethodArgumentNotValidException e) {
+        return WebResponseDto.failure(e.getBindingResult().getFieldError().getDefaultMessage());
     }
 }
