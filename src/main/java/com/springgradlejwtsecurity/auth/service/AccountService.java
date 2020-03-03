6e4735bd72d4ea0de0f83a5a452a82eca19e8ad0
martinyounghoonkim <hoons0131@gmail.com>
2020-03-03 23:51:58 +0900

    로그인 기능에 대한 jwt token 유틸 함수 추가

--- src/main/java/com/springgradlejwtsecurity/auth/service/AccountService.java
+++ src/main/java/com/springgradlejwtsecurity/auth/service/AccountService.java
@@ -1,11 +1,12 @@
 package com.springgradlejwtsecurity.auth.service;
 
-import com.springgradlejwtsecurity.auth.configuration.JwtTokenProvider;
 import com.springgradlejwtsecurity.auth.dto.AuthDto;
 import com.springgradlejwtsecurity.auth.dto.SignUpDto;
 import com.springgradlejwtsecurity.auth.entity.Account;
 import com.springgradlejwtsecurity.auth.exception.CustomException;
 import com.springgradlejwtsecurity.auth.repository.AccountRepository;
+import com.springgradlejwtsecurity.auth.util.JwtTokenClaim;
+import com.springgradlejwtsecurity.auth.util.JwtTokenUtils;
 import lombok.AllArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -19,7 +20,6 @@ import java.util.Optional;
 @AllArgsConstructor
 @Service
 public class AccountService {
-    private final JwtTokenProvider jwtTokenProvider;
     private final PasswordEncoder passwordEncoder;
     private final AccountRepository accountRepository;
 
@@ -46,7 +46,11 @@ public class AccountService {
             throw new CustomException("비밀번호가 일치하지 않습니다.");
         }
 
-        String accessToken = jwtTokenProvider.createToken(account);
+        JwtTokenClaim jwtTokenClaim = JwtTokenClaim.builder()
+                .userId(account.getUserId())
+                .permission(account.getPermission())
+                .build();
+        String accessToken = JwtTokenUtils.createToken(jwtTokenClaim);
         setAuthenticationCookie(accessToken, request, response);
 
         return accessToken;
